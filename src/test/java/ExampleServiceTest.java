2cee88c2b630f6f22800b252092e52f18172bcf5
Thiago Roberto Gaspar Pinto <trgpwild@gmail.com>
2013-07-31 16:35:10 -0300

    ...

---
+++ src/test/java/ExampleServiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleServiceTest {
+
+	@Test
+	public void addTest() {
+		double result = ExampleService.add(1, 1);
+		Assert.assertEquals(
+			Double.doubleToLongBits(2), 
+			Double.doubleToLongBits(result)
+		);
+	}
+	
+}
