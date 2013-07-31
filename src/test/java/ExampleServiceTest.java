866490ab78e4ad15a6874b1167c268e5b2fa7b40
Thiago <trgpwild@gmail.com>
2013-07-31 19:32:54 -0300

    ...

--- src/test/java/ExampleServiceTest.java
+++ src/test/java/ExampleServiceTest.java
@@ -12,4 +12,13 @@ public class ExampleServiceTest {
 		);
 	}
 	
+	@Test
+	public void subtractTest() {
+		double result = ExampleService.subtract(1, 1);
+		Assert.assertEquals(
+			Double.doubleToLongBits(0), 
+			Double.doubleToLongBits(result)
+		);
+	}
+
 }
